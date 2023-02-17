446826fece3cf60112d262d5c804db5e387057fa
Chandler Sutherland <chandlersutherland@berkeley.edu>
2023-02-17 10:49:32 -0800

    fastqc

---
+++ fastqc_multiqc.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=fastqc
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_lowprio
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=02:00:00
+#SBATCH --mail-user=chandlersutherland@berkeley.edu
+#SBATCH --mail-type=ALL
+#SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
+#SBATCH --output=/global/home/users/chandlersutherland/slurm_stdout/slurm-%j.out
+
+module load python 
+module load fastqc 
+source activate e16 
+
+SCRATCH_DIR=/global/scratch/users/chandlersutherland/e16
+OUTPUT_DIR=$SCRATCH_DIR/fastqc
+
+mkdir -p $OUTPUT_DIR
+
+cd $SCRATCH_DIR
+FILES=$(find . -type f -name '*fastq' -print)
+
+fastqc -o $OUTPUT_DIR $FILES 
+
+multiqc -f $OUTPUT_DIR
