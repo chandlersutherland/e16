633566efa320bf49016e11b736ae87aab269d26b
Chandler Sutherland <chandlersutherland@berkeley.edu>
2023-02-28 13:35:59 -0800

    archiving old scripts

--- archive/fastqc_multiqc.bash
+++ archive/fastqc_multiqc.bash
@@ -1,17 +1,27 @@
 #!/bin/bash
-#SBATCH --job-name=nam_genome_download
+#SBATCH --job-name=fastqc
 #SBATCH --partition=savio4_htc
 #SBATCH --qos=minium_htc4_normal
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=03:00:00
+#SBATCH --time=06:00:00
 #SBATCH --mail-user=chandlersutherland@berkeley.edu
 #SBATCH --mail-type=ALL
 #SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
 #SBATCH --output=/global/home/users/chandlersutherland/slurm_stdout/slurm-%j.out
 
 module load python 
-module load pigz 
+module load fastqc 
+source activate e16 
 
-python /global/home/users/chandlersutherland/e16/nam_download.py 
+SCRATCH_DIR=/global/scratch/users/chandlersutherland/e16
+OUTPUT_DIR=$SCRATCH_DIR/fastqc
 
+mkdir -p $OUTPUT_DIR
+
+cd $SCRATCH_DIR
+FILES=$(find . -type f -name '*fastq' -print)
+
+fastqc -o $OUTPUT_DIR -t 24 $FILES 
+
+multiqc -f $OUTPUT_DIR
---
+++ archive/methylation/bismark_test.bash
@@ -0,0 +1,77 @@
+#!/bin/bash
+#SBATCH --job-name=bismark_test
+#SBATCH --partition=savio2
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=09:00:00
+#SBATCH --mail-user=chandlersutherland@berkeley.edu
+#SBATCH --mail-type=ALL
+#SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
+#SBATCH --output=/global/home/users/chandlersutherland/slurm_stdout/slurm-%j.out
+
+module load python 
+module load bowtie2
+module load Bismark
+module load samtools
+module load parallel 
+
+trim_output=$base/$sample/bs_fastq_files
+genome=$base/$sample/genome
+#need to export sample and base 
+#FILES=$(find . -type f -name '*_1.fq' -print)
+#ACCESSIONS=$(basename -a -s _1_val_1.fq $FILES)
+#echo "beginning bismark on sample $sample"
+
+bismark_output=$base/$sample/bismark_test
+
+#just test one 
+ACC='SRR12463972'
+
+time bismark \
+	--temp_dir $SCRATCH \
+	--output_dir $bismark_output \
+	--prefix nondirectional.trim1 \
+	--parallel 4 \
+	--non_directional \
+	$genome \
+	-1 $trim_output/trimmed/${ACC}_1_val_1.fq -2 $trim_output/trimmed/${ACC}_2_val_2.fq
+	
+time bismark \
+	--temp_dir $SCRATCH \
+	--output_dir $bismark_output \
+	--parallel 4 \
+	--prefix directional.trim1 \
+	$genome \
+	-1 $trim_output/trimmed/${ACC}_1_val_1.fq -2 $trim_output/trimmed/${ACC}_2_val_2.fq
+
+time bismark \
+	--temp_dir $SCRATCH \
+	--output_dir $bismark_output \
+	--prefix nondirectional.trim2 \
+	--parallel 4 \
+	--non_directional \
+	$genome \
+	-1 $trim_output/trimmed2/${ACC}_1_val_1.fq -2 $trim_output/trimmed2/${ACC}_2_val_2.fq
+	
+time bismark \
+	--temp_dir $SCRATCH \
+	--output_dir $bismark_output \
+	--parallel 4 \
+	--prefix directional.trim2 \
+	$genome \
+	-1 $trim_output/trimmed2/${ACC}_1_val_1.fq -2 $trim_output/trimmed2/${ACC}_2_val_2.fq
+	
+#export genome=$genome
+#export bismark_output=$bismark_output
+#export -f da_biz 
+
+#make a final report 
+cd $base/$sample/bismark_test
+reports=$(find . -type f -name '*_PE_report.txt' -print)
+for f in $reports 
+	do 
+		echo $f >> conglom_report.txt
+		cat $f | grep -A 5 'Number of sequence pairs with unique' >> conglom_report.txt 
+		cat $f | grep -A 5 'C methylated in CpG context:' >> conglom_report.txt
+done 
