e6110bf55468b240cb21f2e2b2a0430dda2f41e2
Chandler Sutherland <chandlersutherland@berkeley.edu>
2023-03-14 17:13:34 -0700

    samtools coverage of NLRs

---
+++ methylation/samtools_coverage.py
@@ -0,0 +1,27 @@
+import os 
+import sys
+import pandas as pd 
+
+#read in NLR bed file, and take bam file from bash input 
+NLR_position=pd.read_csv(sys.argv[1], sep='\t', names=['Chromosome', 'start', 'end', 'marker', 'code'], index_col=False)
+print('NLR_position file loaded')
+
+bismark_file=str(sys.argv[2])
+print('starting bismark file '+bismark_file)
+basename=bismark_file.split('/')[-1].replace('.bam', '')
+print(basename) 
+
+#create header 
+os.system("echo 'rname  startpos        endpos  numreads        covbases        coverage        meandepth       meanbaseq       meanmapq' >> "+basename+"_coverage.tsv")
+
+print('starting for loop')
+
+#loop the coverage function over the chromosome coordinates given in the NLR file 
+for i in range(1, len(NLR_position)):
+    chromosome=NLR_position.iloc[i,0]
+    start=NLR_position.iloc[i,1]
+    end=NLR_position.iloc[i,2]
+    call=chromosome +':' + str(int(start)) + '-' + str(int(end))
+    os.system('samtools coverage -r '+call+' '+bismark_file+'  >> '+basename+'_coverage.tsv')
+
+os.system("sed '/^#/d' "+basename+"_coverage.tsv > "+basename+"_clean_coverage.tsv") 
